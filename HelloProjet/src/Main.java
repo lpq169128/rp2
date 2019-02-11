bd073981641a614bb3890f1cbc2627defd57b681
lpq169128 <542245193@qq.com>
2019-02-11 14:22:10 +0800

    提交信息

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,4 +6,11 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
+
+    public  void test2(){
+        System.out.println("测试添加数据");
+    }
+    public void  test3(){
+        System.out.println("再次添加数据");
+    }
 }
